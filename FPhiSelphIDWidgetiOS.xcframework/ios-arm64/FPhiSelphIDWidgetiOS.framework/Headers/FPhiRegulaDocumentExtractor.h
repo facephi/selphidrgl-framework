043e96302938cc9370d385216a4863be622b80f8
José Antonio Juan <joseantoniojuan@facephi.com>
2024-08-27 15:39:56 +0200

    Added temporal frameworks

---
+++ FPhiSelphIDWidgetiOS.xcframework/ios-arm64/FPhiSelphIDWidgetiOS.framework/Headers/FPhiRegulaDocumentExtractor.h
@@ -0,0 +1,9 @@
+#ifndef FPhiRegulaDocumentExtractor_h
+#define FPhiRegulaDocumentExtractor_h
+
+#import "IFPhiDocumentExtractor.h"
+
+
+@interface FPhiRegulaDocumentExtractor : IFPhiDocumentExtractor
+@end
+#endif
